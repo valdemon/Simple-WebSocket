57f7c78b1b82824be0abb3a9cfee01892992800d
Kozba, Waldek - Externer Mitarbeiter <Waldek.Kozba.ext@bauermedia.com>
2016-12-08 08:18:53 +0100

    some b. logic

--- server/index.js
+++ server/index.js
@@ -1,7 +1,12 @@
 var express = require('express'),
     app     = express(),
+    var lambda = new aws.Lambda({
+      region: 'us-east-2' //change to your region
+    });
     http    = require('http').createServer(app),
     io      = require('socket.io')(http);
+
+const url = "http://www.supremenewyork.com/shop/all/sweatshirts";
 
 http.listen(3000);
 
@@ -19,9 +24,18 @@ io.on('connection', function (socket) {
     });
 
     socket.on('test', function (data) {
+      http.get(data.url, function(res) {
         io.emit('test response', {
-            success  : true,
-            received : data
-        });
+              success  : true,
+              status: res.statusCode,
+              received : res.body
+          });
+      }).on('error', function(e) {
+        //console.log("Got error: " + e.message);
+        io.emit('test response', {
+              success  : false,
+              received : e.message
+          });
+      });
     });
 });
